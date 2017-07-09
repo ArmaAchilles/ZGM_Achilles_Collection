0e02ee2f8608ba7926aa4000ece748e28e60de42
Kex <rc_mapper@yahoo.com>
2017-07-10 00:38:19 +0200

    fixed

--- src/achilles_pack.sh
+++ src/achilles_pack.sh
@@ -1,6 +1,6 @@
 template_folder="ZGM-17_Achilles_Blufor.Altis"
 side_name_list=(blufor opfor greenfor)
-map_name_list=(Stratis Tanoa Malden Bukovina Bystrica Chernarus Chernarus_Summer Desert Desert_Island Everon Kolgujev Malden Nogova Porto Proving_Grounds Rahmadi Sahrani Shapur Southern_Sahrani Takistan Takistan_Mountains United_Sahrani Utes Zargabad )
+map_name_list=(Stratis Tanoa Malden Bukovina Bystrica Chernarus Chernarus_Summer Desert Desert_Island Everon Kolgujev Malden Nogova Porto Proving_Grounds Rahmadi Sahrani Shapur Southern_Sahrani Takistan Takistan_Mountains United_Sahrani Utes Zargabad)
 
 
 function Achilles_Bash_fnc_genMission {
@@ -17,7 +17,7 @@ function Achilles_Bash_fnc_genMission {
 	folder_prefix=ZGM-17_Achilles
 	image_name_prefix=achilles
 	name_prefix="Zeus 16+1 Achilles"
-	map_name_list=(Stratis Tanoa Malden Bukovina Bystrica Chernarus Chernarus_Summer Desert Desert_Island Everon Kolgujev Malden Nogova Porto Proving_Grounds Rahmadi Sahrani Shapur Southern_Sahrani Takistan Takistan_Mountains United_Sahrani Utes Zargabad )
+	map_name_list=(Stratis Tanoa Malden Bukovina Bystrica Chernarus Chernarus_Summer Desert Desert_Island Everon Kolgujev Malden Nogova Porto Proving_Grounds Rahmadi Sahrani Shapur Southern_Sahrani Takistan Takistan_Mountains United_Sahrani Utes Zargabad)
 	map_postfix_list=(Stratis Tanoa Malden Bootcamp_ACR Woodland_ACR Chernarus Chernarus_Summer Desert_E Desert_Island eden cain abel noe Porto ProvingGrounds_PMC Intro Sara Shapur_BAF SaraLite Takistan Mountains_ACR Sara_dbe1 Utes Zargabad)
 	side_name_list=(blufor opfor greenfor)
 	side_list=(WEST EAST GUER)
@@ -46,6 +46,6 @@ rm -f "tmp/*"
 nohup AddonBuilder "$(cygpath -w "$PWD/$template_folder")" "$(cygpath -w "$PWD/../pbo")" -temp="$(cygpath -w "$PWD")"
 
 for i_side in "${!side_name_list[@]}"; do
-	printf "%s\n" ${!map_name_list[@]} | xargs -i --max-procs=8 bash -c "Achilles_Bash_fnc_genMission $i_side {}"
+	printf "%s\n" ${!map_name_list[@]} | xargs -i --max-procs=4 bash -c "Achilles_Bash_fnc_genMission $i_side {}"
 done
 echo Procedure completed!
