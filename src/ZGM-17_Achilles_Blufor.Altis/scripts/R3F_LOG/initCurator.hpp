56b38a1a1af0a1df396a35dc6ac6ad1b1a988ac4
oOKexOo <rc_mapper@yahoo.com>
2018-05-05 05:47:55 +0200

    R3F module: Request selection before opening the dialog

--- src/ZGM-17_Achilles_Blufor.Altis/scripts/R3F_LOG/initCurator.hpp
+++ src/ZGM-17_Achilles_Blufor.Altis/scripts/R3F_LOG/initCurator.hpp
@@ -4,6 +4,13 @@
 	{
 		private _objects = [param [1, objNull, [objNull]]];
 		
+		if (isNull (_objects select 0)) then
+		{
+			_objects = [localize "STR_OBJECTS"] call Achilles_fnc_SelectUnits;
+		};
+		if (isNil "_objects") exitWith {};
+		if (count _objects == 0) exitWith {["Error: No object selected"] call Achilles_fnc_showZeusErrorMessage};
+		
 		private _dialogResults =
 		[
 			"Disable R3F Logistics",
@@ -15,17 +22,10 @@
 		if (count _dialogResults == 0) exitWith {};
 		private _disabled = if (_dialogResults select 0 == 0) then {true} else {false};
 		
-		if (isNull (_objects select 0)) then
-		{
-			_objects = [localize "STR_OBJECTS"] call Achilles_fnc_SelectUnits;
-		};
-		if (isNil "_objects") exitWith {};
-		if (count _objects == 0) exitWith {["Error: No object selected"] call Ares_fnc_ShowZeusMessage; playSound "FD_Start_F"};
-		
 		{
 			_x setVariable ["R3F_LOG_disabled", _disabled, true];
 		} forEach _objects;
 		
-		[objNull, format ["Changed R3F Logistics Status of %1 objects", count _objects]] call bis_fnc_showCuratorFeedbackMessage;
+		[format ["Changed R3F Logistics Status of %1 objects", count _objects]] call Ares_fnc_showZeusMessage;
 	}
 ] call Ares_fnc_RegisterCustomModule;
